1901de2d1026583e75ef04f0f60c79b4968a67e9
Omar Lengua <omarns21@gmail.com>
2025-03-06 19:31:52 +0100

    Implement: step 2 completed

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
-# Step 1: A Python module is a file containing code designed to perform specific tasks. Start this project by importing the string module.
+# Step 2: You can access the utilities defined inside the imported module through the dot notation. The dot notation works by appending a dot followed by the utility name to the module name. For example, here's how to access the ascii_lowercase constant:
 
 import string
+
+letters = string.ascii_lowercase
