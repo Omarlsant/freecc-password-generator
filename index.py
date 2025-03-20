afc750a19160921383ffeef0e9c479ad1a9e7cca
Omar Lengua <omarns21@gmail.com>
2025-03-20 12:23:22 +0100

    Implement: step 68 completed

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
-# Step 67: As long as all the arguments in a function call are keyword arguments, the order of the arguments doesn't matter. To confirm this, try to change the order of length=8 and nums=1 in your function call.
+# Step 68: Modify your function declaration to take default parameters. Use 16 for the length and 1 for the other constraints.
 
 import re
 import secrets
 import string
 
-def generate_password(length, nums, special_chars, uppercase, lowercase):
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
     # Define the possible characters for the password.
     letters = string.ascii_lowercase
     digits = string.digits
